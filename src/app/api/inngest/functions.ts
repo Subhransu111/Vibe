795ac05217aea51d0c9a7d4bf0fb42cf93e48aa0
Subhransu <subhransutripathy05@gmail.com>
2025-08-04 23:24:58 +0530

    updated the model

--- src/app/api/inngest/functions.ts
+++ src/app/api/inngest/functions.ts
@@ -1,14 +1,26 @@
+import {  openai,grok, createAgent } from "@inngest/agent-kit";
 
 import {inngest} from "../../../inngest/client";
-import { groq, createAgent } from "@inngest/agent-kit";
+
 
 export const helloWorld = inngest.createFunction(
     {id:"helloworld"},
     {event: "test/hello.world"},
     async ({event,step}) =>{
-        await step.sleep("wait a moment", "5s");
+      const CodeAgent = createAgent({
+          name: "CodeAgent",
+          system: "You are an expert Next.js developer.  You write readable and maintainable code . You write simple NextBuildContext.js & React in snippets ",
+          model: grok({ model: "grok-2-latest" }), 
+        });
+
+        const {output}= await CodeAgent.run(
+            `Write the following snippets: ${event.data.value}`
+        );
+
+        console.log(output);
+   
+
         
 
-        return {message: `Hello, ${event.data.value}!`};
-    }
-)
+    return {Success:"ok", summary: output};
+     });
--- src/trpc/router/_app.ts
+++ src/trpc/router/_app.ts
@@ -22,12 +22,12 @@ export const appRouter = createTRPCRouter({
   createAI: baseProcedure
     .input(
       z.object({
-        text: z.string(),
+        value: z.string(),
       }),
     )
     .query((opts) => {
       return {
-        greeting: `hello ${opts.input.text}`,
+        greeting: `hello ${opts.input.value}`,
       };
     }),
 });
