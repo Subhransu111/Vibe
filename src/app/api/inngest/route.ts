6a8a45c6e34683eb3c7dc80a3b4d86a8d237e8b3
Subhransu <subhransutripathy05@gmail.com>
2025-08-07 21:02:44 +0530

    sandbox commit

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,6 +1,6 @@
 import { serve } from "inngest/next";
 import { inngest } from "../../../inngest/client";
-import { helloWorld } from "./functions";
+import { helloWorld } from '@/inngest/function';
 
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
--- src/inngest/function.ts
+++ src/inngest/function.ts
@@ -1,12 +1,18 @@
 import {  openai,grok, createAgent } from "@inngest/agent-kit";
-
-import {inngest} from "../../../inngest/client";
+import {Sandbox} from "@e2b/code-interpreter";
+import {inngest} from "@/inngest/client";
+import { getsandbox } from "./utils";
 
 
 export const helloWorld = inngest.createFunction(
     {id:"helloworld"},
     {event: "test/hello.world"},
     async ({event,step}) =>{
+
+        const SandboxID = await step.run ("get-sandbox-id",async()=>{
+            const sandbox = await Sandbox.create("vibe-nextjs-examine-2")
+            return sandbox.sandboxId;
+        })
       const CodeAgent = createAgent({
           name: "CodeAgent",
           system: "You are an expert Next.js developer.  You write readable and maintainable code . You write simple NextBuildContext.js & React in snippets ",
@@ -17,10 +23,17 @@ export const helloWorld = inngest.createFunction(
             `Write the following snippets: ${event.data.value}`
         );
 
-        console.log(output);
+
+        const sandboxURL = await step.run("get-sandbox-url",async()=>{
+            const sandbox = await getsandbox(SandboxID);
+            const host = sandbox.getHost(3000);
+            return `https://${host}`;
+
+        })
+
    
 
         
 
-    return {Success:"ok", summary: output};
+    return {output, sandboxURL};
      });
